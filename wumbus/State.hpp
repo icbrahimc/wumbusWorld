61d444b140b43d3567582267905626af761bcb8c
icbrahimc <ibrahimconteh@Ibrahims-MacBook-Pro.local>
2017-04-03 18:50:37 -0400

    Added the getters and setters for the state class

--- wumbus/State.hpp
+++ wumbus/State.hpp
@@ -11,4 +11,31 @@
 
 #include <stdio.h>
 
+enum status { NO = -1, POSS = 0, YES = 1};
+
+class State {
+private:
+    status breeze;
+    status glitter;
+    status pit;
+    status stench;
+    status wumbus;
+public:
+    // Constructor.
+    State();
+    
+    // Change the status of the five states.
+    void setBreeze(status input) { breeze = input; };
+    void setGlitter(status input) { glitter = input; };
+    void setPit(status input) { pit = input; };
+    void setStench(status input) { stench = input; };
+    void setWumbus(status input) { wumbus = input; };
+    
+    // Return the status of the particular state.
+    bool isBreeze() const { return this->breeze == YES; };
+    bool isGlitter() const { return this->glitter == YES;};
+    bool isPit() const { return this->pit == YES; };
+    bool isStench() const { return this->stench == YES; };
+    bool isWumbus() const { return this->wumbus == YES; };
+};
 #endif /* State_hpp */
