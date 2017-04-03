d4465b4be1dae9035edfca3952f8aa2f4b95e9e7
icbrahimc <ibrahimconteh@Ibrahims-MacBook-Pro.local>
2017-04-03 18:29:50 -0400

    Added the state object to add into the world

---
+++ wumbus/State.cpp
@@ -0,0 +1,9 @@
+//
+//  State.cpp
+//  wumbus
+//
+//  Created by Ibrahim Conteh on 4/3/17.
+//  Copyright © 2017 Ibrahim Conteh. All rights reserved.
+//
+
+#include "State.hpp"
---
+++ wumbus/State.hpp
@@ -0,0 +1,14 @@
+//
+//  State.hpp
+//  wumbus
+//
+//  Created by Ibrahim Conteh on 4/3/17.
+//  Copyright © 2017 Ibrahim Conteh. All rights reserved.
+//
+
+#ifndef State_hpp
+#define State_hpp
+
+#include <stdio.h>
+
+#endif /* State_hpp */
