81d1678feff2e8f608b40c090c63e9f9a0f2bc6c
icbrahimc <ibrahimconteh@Ibrahims-MacBook-Pro.local>
2017-04-03 18:24:13 -0400

    Added the world object

---
+++ wumbus/World.hpp
@@ -0,0 +1,21 @@
+//
+//  World.hpp
+//  wumbus
+//
+//  Created by Ibrahim Conteh on 4/3/17.
+//  Copyright © 2017 Ibrahim Conteh. All rights reserved.
+//
+
+#ifndef World_hpp
+#define World_hpp
+
+#include <stdio.h>
+#include <vector>
+
+class World {
+private:
+    
+public:
+};
+
+#endif /* World_hpp */
